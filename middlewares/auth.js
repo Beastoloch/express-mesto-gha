431a9a0853a0bd659f36dc78ad7a62bd5916a0e5
Beastoloch <maks.serebrov.94@gmail.com>
2024-02-03 04:19:05 +0700

    fix: фиксы после ревью

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { NODE_ENV, JWT_SECRET } = process.env;
 const { ERROR_AUTH_CODE } = require('../utility/constants');
 
 const handleAuthError = (res) => {
@@ -20,7 +21,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'test-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return handleAuthError(res);
   }
