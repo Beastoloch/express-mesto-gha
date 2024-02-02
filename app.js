5f6fdb789c1c787fabf3b8b2c51fe62d0f2864df
Beastoloch <maks.serebrov.94@gmail.com>
2024-02-03 01:02:46 +0700

    fix: фиксы после ревью

--- app.js
+++ app.js
@@ -33,17 +33,14 @@ app.use(limiter);
 app.use(function(req, res, next) {
   const { origin } = req.headers;
   const { method } = req;
-  console.log(origin);
-  console.log(allowedCors);
-  console.log(allowedCors.includes(origin));
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     return res.end();
-  }
-  if (allowedCors.includes(origin)) {
-    console.log('aboba');
-    res.header('Access-Control-Allow-Origin', origin);
   }
 
   next();
